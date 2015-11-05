8253ec80a313ee04a3e7b6d17026a349ff23cde3
Alexie Papanicolaou <alpapan@gmail.com>
2015-11-05 12:25:57 +1100

    test suite typos

--- test_suite/runme.sh
+++ test_suite/runme.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # the data where prepared using transdecoder
-echo "Predicting proteins"
-../3rd_party/transdecoder/transcripts_to_best_scoring_ORFs.pl -t test.fsa
+echo "Normally we use PASA but for this demo we will just use TransDecoder for quickly predicting proteins"
+../3rd_party/transdecoder/TransDecoder.LongOrfs -t test.fsa
+../3rd_party/transdecoder/TransDecoder.Predict -t test.fsa
 
 # prepare genome. Softmasked using RepeatMasker's GFF and bedtools
 bunzip2 -k dmel-X-r5.53.repeat.hard.bz2 dmel-X-r5.53.repeat.soft.bz2
@@ -10,8 +11,8 @@ bunzip2 -k dmel-X-r5.53.repeat.hard.bz2 dmel-X-r5.53.repeat.soft.bz2
 # predict genes. Don't produce genome annotation (not installed by default)
 echo "Predicting genes"
 ../bin/prepare_golden_genes_for_predictors.pl -genome dmel-X-r5.53.repeat.hard -softmasked dmel-X-r5.53.repeat.soft -same_species \
- -transdecoder_gff test.fsa.transdecoder.gff3 -transdecoder_peptides test.fsa.transdecoder.pep -transdecoder_assembly test.fsa \
- -threads 2 -augustus ../3rd_party/augustus -norefine # options for speed/this demo
+ -peptide test.fsa.transdecoder.pep \
+ -threads 2 -augustus ../3rd_party/augustus -norefine
 
 find . -type f -empty -delete
 
