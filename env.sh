1e317e7d12f4097b7e11bc2826cd80cf1e3ee720
Alexie Papanicolaou <alpapan@gmail.com>
2016-01-03 15:50:31 +1100

    repeatmasker var

--- env.sh
+++ env.sh
@@ -2,20 +2,23 @@
 echo What is the PATH for JAMg? e.g. `pwd`
 read JAMG_PATH
 
-echo What is the maximum intron length? e.g. 70000
-read MAX_INTRON_LENGTH
-
-echo What is the path to the genome?
-read GENOME_PATH
-
-echo What is the name you want to give to the genome - computer friendly, no spaces?
-read GENOME_NAME
-
 echo How many CPUs would like to use for some of the multi-threaded scripts? e.g. 5
 read LOCAL_CPUS
 
 echo How much memory would you like to be able to use? In Gigabytes e.g. 50
 read MAX_MEMORY_G
+
+echo What is the name you want to give to the genome - computer friendly, no spaces?
+read GENOME_NAME
+
+echo What is the path to the genome?
+read GENOME_PATH
+
+echo What is the maximum intron length? e.g. 70000
+read MAX_INTRON_LENGTH
+
+echo What is a species classification for RepeatMasker (e.g. Insecta, fungi, Viridiplantae, human, primates)
+read SPECIES_CATEGORY
 
 #system variables
 LD_LIBRARY_PATH=$JAMG_PATH/3rd_party/lib:$JAMG_PATH/3rd_party/lib64:$JAMG_PATH/3rd_party/mysql/lib:$JAMG_PATH/3rd_party/lib:$JAMG_PATH/3rd_party/lib64:$JAMG_PATH/3rd_party/mysql/lib:$LD_LIBRARY_PATH
@@ -26,13 +29,19 @@ PATH=$JAMG_PATH/bin:$JAMG_PATH/3rd_party/bin:$PATH
 
 echo " 
 #!/bin/bash
-export JAMG_PATH=$JAMG_PATH
-export MAX_INTRON_LENGTH=$MAX_INTRON_LENGTH
-export GENOME_PATH=$GENOME_PATH
+
+# species specific (can edit these)
 export GENOME_NAME=$GENOME_NAME
+export GENOME_PATH=$GENOME_PATH
+export MAX_INTRON_LENGTH=$MAX_INTRON_LENGTH
+export SPECIES_CATEGORY=$SPECIES_CATEGORY
+
+# system speficic
+export JAMG_PATH=$JAMG_PATH
 export LOCAL_CPUS=$LOCAL_CPUS
 export MAX_MEMORY_G=$MAX_MEMORY_G
 
+# compilation variables
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
 export LDFLAGS=\"$LDFLAGS\"
