329be8326e11c6c3488492b3d083ff678dbc8f20
Alexie Papanicolaou <alpapan@gmail.com>
2016-01-09 15:57:43 +1100

    augustus_RNAseq_hints.pl speedups; still untested

--- bin/augustus_RNAseq_hints.pl
+++ bin/augustus_RNAseq_hints.pl
@@ -20,10 +20,13 @@ Mandatory options:
 Other options:
 
  -strandness       i		If RNAseq is directional, provide direction: 0 for unknown (default); or 1 for + strand; -1 for - strand
- -min_score        i  		Minimum score for parsing (defaults to 20)
+ -min_coverage     i	  	Minimum coverage for parsing coverage (defaults to 20)
  -window           i  		Window size for coverage graph (defaults to 50)
  -background_fold  i  		Background (defaults to 4), see perldoc
  -no_hints            		Don't create hints file for Augustus, just process junction reads
+ -cpus             i		Number of CPUs for sorting (defaults to 4)
+ -min_jr_length    i		Minimum read length to use for junctions (def to 75)
+ -min_jr_score     i		Minimum MAQ score to use for junctions (def to 34)
 
 =head1 DESCRIPTION
 
@@ -56,6 +59,7 @@ use File::Basename;
 use FindBin qw($RealBin);
 use lib ("$RealBin/../PerlLib");
 $ENV{PATH} .= ":$RealBin:$RealBin/../3rd_party/bin/";
+use Fasta_reader;
 
 my ( $samtools_exec, $bedtools_exec, $bed_to_aug_script ) = &check_program( 'samtools', 'bedtools','bed12_to_augustus_junction_hints.pl' );
 
@@ -65,19 +69,23 @@ my ( $samtools_exec, $bedtools_exec, $bed_to_aug_script ) = &check_program( 'sam
 my ( @bamfiles, $genome, $help,$no_hints );
 my $cpus = 4;
 my $window           = 50;
-my $min_score        = 20;
+my $min_coverage        = 20;
+my $min_jr_score        = 34;
+my $min_jr_length       = 75;
 my $strandness       = int(0);
 my $background_level = 4;
 pod2usage $! unless &GetOptions(
             'help'              => \$help,
             'bam|in:s{,}'       => \@bamfiles,
             'genome|fasta:s'    => \$genome,
-            'min_score:i'       => \$min_score,
+            'min_coverage:i'    => \$min_coverage,
             'strandness:i'      => \$strandness,
             'window:i'          => \$window,
             'background_fold:i' => \$background_level,
 	    'nohints|no_hints'  => \$no_hints,
-	    'cpus:i'            => \$cpus
+	    'cpus:i'            => \$cpus,
+	    'min_jr_length:i'   => \$min_jr_length,
+	    'min_jr_score:i'    => $min_jr_score
 );
 
 pod2usage if $help;
@@ -116,17 +124,19 @@ if (scalar(@bamfiles == 1)){
 &process_cmd("$samtools_exec faidx $genome") unless -s $genome . '.fai';
 die "Cannot index genome $genome\n" unless -s $genome . '.fai';
 unless (-e "$master_bamfile.junctions.completed"){
- &process_cmd("$samtools_exec rmdup -S $master_bamfile - | $bedtools_exec bamtobed -bed12 | $bed_to_aug_script -prio 7 -out $master_bamfile.junctions.bed"
- ."|sort -S 2G -n -k 4,4 | sort -S 2G -s -n -k 5,5 | sort -S 2G -s -n -k 3,3 | sort -S 2G -s -k 1,1 -o $master_bamfile.junctions.hints" ) unless -s "$master_bamfile.junctions.hints";
+ my $junction_bam = &grab_intronic_bam($master_bamfile);
+ if (-f $junction_bam && (-s $junction_bam) > 10000){
+ &process_cmd("$bedtools_exec bamtobed -bed12 < $junction_bam | $bed_to_aug_script -prio 7 -out $master_bamfile.junctions.bed"
+ ."|sort -S 2G -n -k 4,4 | sort -S 2G -s -n -k 5,5 | sort -S 2G -s -n -k 3,3 | sort -S 2G -s -k 1,1"
+ ." -o $master_bamfile.junctions.hints" ) unless (-s "$master_bamfile.junctions.hints");
+
  unless ($no_hints){
 	 # For Augustus
-	 &only_keep_intronic("$master_bamfile.junctions.hints");
+	 &intron_driven_fixes("$master_bamfile.junctions.hints");
 	 # don't merge before getting intronic.
 	 &merge_hints("$master_bamfile.junctions.hints");
- }
- # For JBrowse
- &process_cmd("$bedtools_exec bedtobam -bed12 -g $genome.fai -i $master_bamfile.junctions.bed| $samtools_exec sort -m 4G -@ 4 -o $master_bamfile.junctions.bam -") unless -s "$master_bamfile.junctions.bam";
- &process_cmd("$samtools_exec index $master_bamfile.junctions.bam");
+  }
+ }else{	warn "No junction reads found!"; }
  &touch("$master_bamfile.junctions.completed");
 }
 
@@ -151,6 +161,7 @@ if (    -e "$master_bamfile.junctions.completed"
    &merge_hints("$master_bamfile.rnaseq.hints");
    &touch("$master_bamfile.rnaseq.completed");
  }
+
  print "Done!\n";
 }
 elsif (!$no_hints) {
@@ -191,7 +202,7 @@ sub bg2hints() {
  while ( my $ln = <IN> ) {
   chomp($ln);
   my @data = split( "\t", $ln );
-  next unless $data[3] >= $min_score;
+  next unless $data[3] >= $min_coverage;
   # store data in an array
   for ( my $i = $data[1] ; $i <= $data[2] ; $i++ ) {
    # co-ords in bg are 0-based; hints/gff is 1-based
@@ -230,8 +241,7 @@ sub bg2hints() {
    }
 #   next if scalar(@splice) < ( $window / 2 );
    my $median = &median( \@splice );
-   $median = $splice[0] if !$median;
-   next unless $median && $median >= $min_score;
+   next unless $median && $median >= $min_coverage;
    print OUT $ref
      . "\tRNASeq\texonpart\t"
      . $i . "\t"
@@ -249,24 +259,24 @@ sub bg2hints() {
  return $outfile;
 }
 
-sub only_keep_intronic(){
+sub intron_driven_fixes(){
  my $file = shift;
  my %hash;
 
  # get introns (we will postprocess them later)
- open (IN,$file);
- open (OUT1,">".$file.".intron.only");
- while (my $ln=<IN>){
-   print OUT1 $ln if ($ln=~/\tintron\t/);
- }
- close OUT1;
- close IN;
- # group introns and remove solitary ones
- &merge_hints("$file.intron.only");
- &convert_mult_to_score("$file.intron.only");
+ # fix strand of intron based on sequence
+ # keep non-canonical ones (singletons purged later)
+ my $intronic_file = &get_intron_orient($file);
 
- # find exons matching intron 
- open (IN,$file.".intron.only");
+ # group introns based on co-ords
+ &merge_hints($intronic_file);
+ # set score to equal number of introns grouped 
+ # and remove singletons/solitary ones
+ &convert_mult_to_score($intronic_file);
+
+ # find 1 exon pair matching intron that has been grouped
+ # and give it the same score AND strand
+ open (IN,$intronic_file);
  while (my $ln=<IN>){
   my @data = split("\t",$ln);
   if ($data[8]=~/grp=([^;]+)/){
@@ -366,7 +376,9 @@ sub mean() {
 sub median() {
  my $array_ref = shift;
  my @sorted = sort { $a <=> $b } @{$array_ref};
- return $sorted[ int( @sorted / 2 ) ];
+ my $median = $sorted[ int( @sorted / 2 ) ];
+ return $array_ref->[0] if !$median;
+ return $median;
 }
 
 
@@ -377,6 +389,7 @@ sub convert_mult_to_score(){
 	open (OUT,">$file.scored");
 	while (my $ln=<IN>){
 		my @data = split("\t",$ln);
+		# discard singletons
 		if ($data[8] && $data[8]=~/mult=(\d+)/){
 			$data[5] = $1;
 			print OUT join("\t",@data);
@@ -387,3 +400,111 @@ sub convert_mult_to_score(){
 	close OUT;
 	rename("$file.scored",$file);
 }
+
+sub get_intron_orient(){
+	my $hint_file = shift;
+	my $fasta_obj = new Fasta_reader($genome);
+	my %fasta_data = $fasta_obj->retrieve_all_seqs_hash();
+	# use the introns to set orientation for both introns and exons
+	# (this would require parsing the file a couple of time)
+	open (IN,$hint_file) || die;
+	# # spit out splice sites while at it
+	open (SPLICE1,">$hint_file.splice.augustus");
+	open (SPLICE2,">$hint_file.splice.gsnap");
+	open (GFF,">$hint_file.intron.only");
+	my $counter=int(0);
+	while (my $ln=<IN>){
+		my @data = split("\t",$ln);
+		next unless $data[6] && $data[2] eq 'intron';
+		my ($ref,$type,$start,$end,$strand) = ($data[0],$data[2],$data[3],$data[4],$data[6]);
+		die "Cannot find sequence for $ref in genome FASTA file\n" unless $fasta_data{$ref};
+		die "Unexpected GFF with start higher than the end\n$ln" if $start > $end;
+		# acceptor and donor sites (3' and 5' on + strand)
+		my $site1 = uc(substr($fasta_data{$ref},($start-1),2));
+		my $site2 = uc(substr($fasta_data{$ref},($end-1-1),2));
+		if (
+		(($site1 eq 'GT' || $site1 eq 'GC') && $site2 eq 'AG') ||
+		($site1 eq 'AT' && $site2 eq 'AC')
+		 ){
+			$strand = '+';
+		}elsif (
+		(($site2 eq 'AC' || $site2 eq 'GC') && $site1 eq 'CT') ||
+		($site2 eq 'AT' && $site1 eq 'GT')
+		 ){
+			$strand = '-';
+		}else{
+			# leave them unchanged
+			print GFF $ln;
+			next;
+		}
+		$counter++;
+		my $intron_gsnap_txt = ">intron.$counter $ref:$start..$end\n";
+		# sequence 40 bp up and 40 bp upstream
+		my $site1_seq = lc(substr($fasta_data{$ref},($start-1-40),82));
+		my $site2_seq = lc(substr($fasta_data{$ref},($end-1-1-40),82));
+		$site1_seq =~ tr/n/-/;$site2_seq =~ tr/n/-/;
+		if ($strand eq '-'){
+			$site1_seq = &revcomp($site1_seq);
+			$site2_seq = &revcomp($site2_seq);
+			$intron_gsnap_txt = ">intron.$counter $ref:$end..$start\n";
+		}
+		my ($type1,$type2) = ($strand eq '+') ? ('dss','ass') : ('ass','dss');
+		#augustus
+		print SPLICE1 "$type1 $site1_seq\n" if $site1_seq;
+		print SPLICE1 "$type2 $site2_seq\n" if $site2_seq;
+
+		#gsnap
+		print SPLICE2 $intron_gsnap_txt;
+
+		$data[6] = $strand;
+		print GFF join("\t",@data);
+	}
+	close IN;
+	close GFF;
+	close SPLICE1;
+	close SPLICE2;
+	return "$hint_file.intron.only";
+}
+
+sub revcomp {
+  my $dna     = shift;
+  return if !$dna;
+  my $revcomp = reverse($dna);
+  $revcomp =~ tr/ACGTacgt/TGCAtgca/;
+  return $revcomp;
+}
+
+sub grab_intronic_bam(){
+	my $file = shift;
+	my $outfile = $file.".junctions.bam";
+	# at least 10 bp each side and intron at least 10bp.
+	# without \t at end, allows for double introns (cf bed12_to_augustus_junction_hints.pl )
+	&process_cmd("$samtools_exec view -@ $cpus -q $min_jr_score -m $min_jr_length $file |grep -P '[0-9]{2,}M[0-9]{2,}N[0-9]{2,}M'|"
+		." samtools view -T $genome -@ $cpus -b -o $outfile -");
+	&process_cmd("$samtools_exec rmdup -S $outfile - |$samtools_exec sort -m 4G -@ $cpus -o $outfile.sorted -");
+	&process_cmd("$samtools_exec index $outfile.sorted");
+	return $outfile;
+}
+
+
+=pod
+
+=head1 CIGAR Specs
+
+ M 0 alignment match (can be a sequence match or mismatch)
+ I 1 insertion to the reference
+ D 2 deletion from the reference
+ N 3 skipped region from the reference
+ S 4 soft clipping (clipped sequences present in SEQ)
+ H 5 hard clipping (clipped sequences NOT present in SEQ)
+ P 6 padding (silent deletion from padded reference)
+ = 7 sequence match
+ X 8 sequence mismatch
+
+ H can only be present as the first and/or last operation.
+ S may only have H operations between them and the ends of the CIGAR string.
+ For mRNA-to-genome alignment, an N operation represents an intron. For other types of alignments,
+the interpretation of N is not defined.
+ Sum of lengths of the M/I/S/=/X operations shall equal the length of SEQ.
+
+=cut
