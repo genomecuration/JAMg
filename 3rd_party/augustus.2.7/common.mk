ec1468c4f8a03dca3f60979da54b60b9433ebd2c
Alexie Papanicolaou <alpapan@gmail.com>
2016-01-21 17:04:09 +1100

    removed augustus2

--- 3rd_party/augustus.2.7/common.mk
+++
@@ -1,9 +0,0 @@
-# Definitions common to all Makefiles
-# This file is included from other Makefiles in the augustus project.
-
-# uncomment this line to enable comparative gene finding (requires young compiler which supports C++11 standard)
-# COMPGENEPRED = true
-
-# uncomment this line when you need MySQL access to sequences (most users don't)
-# MYSQL = true
-
