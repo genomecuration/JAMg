ea229b613bcb931df09972fcb1af84d35f68388a
Alexie Papanicolaou <alpapan@gmail.com>
2019-02-06 11:11:01 +1100

    genbank requirements update

--- bin/parse_genome_for_ncbi.pl
+++ bin/parse_genome_for_ncbi.pl
@@ -16,10 +16,12 @@ while (my $record=<IN>){
 	my @lines = split("\n",$record);
 	my $id = shift(@lines);
 	my $seq = join("\n",@lines);
-	$seq=~s/^N+//;
-	$seq=~s/N$//;
-        my $Ns = ($seq =~ tr/N//);
+	if ($seq=~/^N+/){
+		die "Sequence $id has Ns in the start of the sequence\n";
+	}
+	$seq=~s/N+$//;
 	next if (length($seq) < 2000);
+        my $Ns = ($seq =~ tr/N//);
 	next if ($Ns / length($seq) > 0.4) && length($seq) < 20000;
 	print OUT ">$id\n$seq\n";
 
